e8ac70b97779cbb4d0abc017c6db7dfd2580fb66
Bryan Hyland <bryan.hyland32@gmail.com>
2018-04-10 13:00:48 -0700

    Lunch 10Apr18

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    [SerializeField] float speed = 5f;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        float moveForward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float moveRight = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        Vector2 move = new Vector2(moveRight, moveForward);
+
+        transform.Translate(move);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour 
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float fireRate;
+    [SerializeField] float bulletSpeed;
+    [SerializeField] float gunOffset;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            InvokeRepeating("Shoot", 0f, fireRate);
+        }
+	}
+
+    void Shoot()
+    {
+        var bullet = Instantiate(bulletPrefab, Vector3.up * gunOffset, Quaternion.identity) as GameObject;
+        bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, bulletSpeed * Time.deltaTime), ForceMode2D.Impulse);
+    }
+}
